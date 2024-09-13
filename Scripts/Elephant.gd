d3baad7cccfc107551352e9900e6f9a96699acb6
cyp <cypriane81@gmail.com>
2024-09-14 00:26:52 +0100

    [#Fixed] Direction

--- Scripts/Elephant.gd
+++ Scripts/Elephant.gd
@@ -1,13 +1,22 @@
 extends RigidBody2D
 
+@export var trunkStart: Node2D
+
 @export var trunkEnd: RigidBody2D
+
+@export var bodies: Array[Texture2D]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$BodySprite.texture = bodies[0]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_pressed("water"):
 		apply_central_impulse(-(trunkEnd.global_position - global_position).normalized() * 1000)
+
+	if linear_velocity.length() > 5:
+		$BodySprite.texture = bodies[1]
+	else:
+		$BodySprite.texture = bodies[0]
